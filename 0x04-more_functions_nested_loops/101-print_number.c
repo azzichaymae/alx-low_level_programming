cac6e6909b8ed67fb4469cc487b1933762a95dc4
azzichaymae <142222368+azzichaymae@users.noreply.github.com>
2023-09-16 01:10:53 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
+ * print_number - Prints an integer.
+ * @n: Input integer.
  */
-
 void print_number(int n)
 {
-  unsigned int k = n;
+	unsigned int k = n;
 
-  if (n < 0) {
-    n *= -1;
-    k = n;
-    _putchar('-');
-  }
+	if (n < 0) {
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
 
-  k /= 10;
+	k /= 10;
 
-  if (k != 0)
-    print_number(k);
+	if (k != 0)
+		print_number(k);
 
-  _putchar((unsigned int) n % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
 }
